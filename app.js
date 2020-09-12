c09e40309056847e67559bd1f97b5dd953a3d53d
boniface193 <bonifacedennis40@yahoo.com>
2020-09-12 06:03:18 -0700

    updated

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ app.use(bodyParser.json())
 
 app.use('/posts', router)
 
+app.use((req, res, next) => {
+    res.header('Access-Contron-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-with, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use((req, res) => {
     res.status(404).send('not available yet')
 })
