49ea6e73cf4b1c26e7a6ee339ebc796e06641b03
boniface193 <bonifacedennis40@yahoo.com>
2020-09-12 06:26:54 -0700

    cors installed

--- app.js
+++ app.js
@@ -3,25 +3,28 @@ const app = express();
 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const router = require('./routers/index');
 require('dotenv/config');
 
 const port = process.env.PORT || 3000;
 
+app.use(cors())
+
 app.use(bodyParser.json())
 
 app.use('/posts', router)
 
-app.use((req, res, next) => {
-    res.header('Access-Contron-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-with, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE');
-        return res.status(200).json({});
-    }
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Contron-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Header', 'Origin, X-Requested-with, Content-Type, Accept, Authorization');
+//     if (req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE');
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
 
 app.use((req, res) => {
     res.status(404).send('not available yet')
